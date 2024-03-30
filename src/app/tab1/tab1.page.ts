d2021e4ba4fda15645a00f6afce282d85739dfe4
NasDev07 <nasruddin20182019@gmail.com>
2024-03-30 14:58:59 +0700

    Mengenal Component lonic

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,12 +1,36 @@
 import { Component } from '@angular/core';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
-  styleUrls: ['tab1.page.scss']
+  styleUrls: ['tab1.page.scss'],
 })
 export class Tab1Page {
+  constructor(public alertController: AlertController) {}
 
-  constructor() {}
+  async presentAlertConfirm() {
+    const alert = await this.alertController.create({
+      cssClass: 'my-custom-class',
+      header: 'Message text!!!',
+      buttons: [
+        {
+          text: 'Cance',
+          role: 'cancel',
+          cssClass: 'secondary',
+          handler: (blah) => {
+            console.log('Confirm cancel: blah');
+          },
+        },
+        {
+          text: 'okey',
+          handler: () => {
+            console.log('Confirm okay');
+          },
+        },
+      ],
+    });
 
+    await alert.present();
+  }
 }
